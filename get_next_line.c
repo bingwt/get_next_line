1f0656d26c1a3c5c041f6317ad54d28db162f4b6
Bing Wen Tan <btan@c2r5s4.42singapore.sg>
2023-10-13 15:52:46 +0800

    modified:   get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,20 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:24:40 by btan              #+#    #+#             */
-/*   Updated: 2023/10/11 01:54:19 by btan             ###   ########.fr       */
+/*   Updated: 2023/10/13 15:52:05 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
 char	*get_next_line(int fd)
 {
+	int
+
+/*char	*get_next_line(int fd)
+{
 	int	buffer_index;
-	static int	read_bytes;
+	int	read_bytes;
 	char	buffer[BUFFER_SIZE];
 	char	*line;
 	
@@ -38,7 +41,7 @@ char	*get_next_line(int fd)
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
 	}
 	return (line);
-}
+}*/
 /*{
 	char	buffer[BUFFER_SIZE];
 	char	*ptr;
@@ -120,7 +123,8 @@ char	*get_next_line(int fd)
 	char	buffer[BUFFER_SIZE];
 	char	*temp;
 
-	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	tne);
+	emp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!temp)
 		return (NULL);
 	line = "";
@@ -132,7 +136,7 @@ char	*get_next_line(int fd)
 			if (buffer[i] == '\n')
 			{
 				free(temp);
-				return (line);
+		e	renurn (line);
 			}
 			temp[i] = buffer[i];
 			i++;
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:24:48 by btan              #+#    #+#             */
-/*   Updated: 2023/10/11 01:55:08 by btan             ###   ########.fr       */
+/*   Updated: 2023/10/13 15:52:39 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <stdlib.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE	20
+# define BUFFER_SIZE	1
 # endif
 
 size_t	ft_strlen(const char *s);
