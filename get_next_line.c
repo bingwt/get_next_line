3da5035e3ca2491cbf089f37579bcd59e5adde36
Bing Wen Tan <btan@c2r5s4.42singapore.sg>
2023-10-23 00:25:01 +0800

    modified:   get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,20 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:24:40 by btan              #+#    #+#             */
-/*   Updated: 2023/10/22 23:56:21 by btan             ###   ########.fr       */
+/*   Updated: 2023/10/23 00:24:53 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	int			read_bytes;
+
+	if (fd >= 0 && BUFFER_SIZE > 0)
+	{
+		if (!buffer)
+			ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+			read_bytes = read(fd, buffer, BUFFER_SIZE);
+}
