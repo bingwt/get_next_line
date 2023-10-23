51ab2ac16562e1168b598e57565dfec05674690d
Bing Wen Tan <btan@c2r5s4.42singapore.sg>
2023-10-23 16:07:34 +0800

    modified:   get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:24:40 by btan              #+#    #+#             */
-/*   Updated: 2023/10/23 14:14:49 by btan             ###   ########.fr       */
+/*   Updated: 2023/10/23 15:43:30 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_newline(char	*buffer)
+static char	*get_line(char	*buffer)
 {
 	int		len;
 	char	*temp;
@@ -42,7 +42,7 @@ char	*get_next_line(int fd)
 		buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
 	}
-	while (ft_strchr(buffer, '\n') == NULL && read_bytes)
+	while (ft_strchr(buffer, '\n') == NULL)
 	{
 		temp = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 		read_bytes = read(fd, temp, BUFFER_SIZE);
@@ -54,10 +54,12 @@ char	*get_next_line(int fd)
 		buffer = ft_strdup(new);
 		free(new);
 		new = NULL;
+		if (read_bytes < 1)
+			break ;
 	}
 	if (ft_strchr(buffer, '\n') != NULL)
 	{
-		new = get_newline(buffer);
+		new = get_line(buffer);
 		temp = ft_strdup(ft_strchr(buffer, '\n'));
 		free(buffer);
 		buffer = NULL;
@@ -83,17 +85,84 @@ char	*get_next_line(int fd)
 	return (new);
 }
 
-#include <fcntl.h>
-#include <stdio.h>
-int	main()
-{
-	int	fd = open("tests/variable_nls.txt", O_RDONLY);
-	char	*line = get_next_line(fd);
+//#include <fcntl.h>
+//#include <stdio.h>
+//int	main()
+//{
+//	int	fd = open("tests/read_error.txt", O_RDONLY);
+//	char	*line = get_next_line(fd);
+//	for (int i = 1; i < 16; i++)
+//	{
+//		printf("Line %d: %s", i, line);
+//		free(line);
+//		line = get_next_line(fd);
+//	}
+//}
 
-	for (int i = 1; i < 16; i++)
-	{
-		printf("Line %d: %s", i, line);
-		free(line);
-		line = get_next_line(fd);
-	}
-}
+// int main(void)
+// {
+//   // Create a temporary file and write test content to it.
+//   char test_content[] = 
+// 						// "0123456789012345678901234567890123456789x0\n"
+// 						// "0123456789012345678901234567890123456789x1\n"
+// 						// "0123456789012345678901234567890123456789x2\n"
+// 						// "0123456789012345678901234567890123456789x3\n"
+// 						// "0123456789012345678901234567890123456789x4\n"
+// 						// "0123456789012345678901234567890123456789x5\n"
+// 						// "\n"
+// 						// "\n"
+// 						// "\n"
+// 							// "0123456789\n"
+// 							// "012345678\n"
+// 							// "90123456789\n" //here ownwards weird
+// 							// "0123456789\n"
+// 							// "xxxx\n"
+
+// 						"\n"
+// 						"0123456789012345678901234567890123456789x2\n"
+// 						"0123456789012345678901234567890123456789x3\n"
+// 						"\n"
+// 						"0123456789012345678901234567890123456789x5\n"
+// 						"\n"
+// 						"\n"
+// 						"0123456789012345678901234567890123456789x8\n"
+// 						"\n"
+// 						"\n"
+// 						"\n"
+// 						"0123456789012345678901234567890123456789x12";
+
+// 	FILE *temp = fopen("temp_test.txt", "w+");
+// 	if (!temp)
+// 	{
+// 		perror("Error creating temporary file");
+// 		return 1;
+// 	}
+// 	fwrite(test_content, sizeof(char), sizeof(test_content) - 1, temp);
+// 	fclose(temp);
+// 	// Open the temporary file to read with get_next_line.
+// 	int fd = open("temp_test.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		perror("Error opening the temporary file");
+// 		return 1;
+// 	}
+// 	int i = 0;
+// 	char *line = NULL;
+// 	// line = get_next_line(fd);
+// 	// printf("res:%s;\n",line);
+// 	while ( (line = get_next_line(fd)) != NULL)
+// 	{
+// 		if (line[0] == '\0')
+// 		{
+// 			free(line);
+// 			break;
+// 		}
+// 		printf("res%d:%s;\n",i ,line);
+// 		free(line);
+// 		i ++;
+// 	}
+// 	close(fd);
+// 	// Optionally, remove the temporary file after testing.
+// 	remove("temp_test.txt");
+// 	return 0;
+// }
