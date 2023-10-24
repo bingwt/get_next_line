c1eb2f600d70ba2415a61f77b70703606626b3f3
Bing Wen Tan <btan@c2r5s4.42singapore.sg>
2023-10-24 23:45:50 +0800

    modified:   get_next_line.c 	modified:   get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:24:55 by btan              #+#    #+#             */
-/*   Updated: 2023/10/24 14:40:18 by btan             ###   ########.fr       */
+/*   Updated: 2023/10/24 15:36:15 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,32 @@ char	*ft_strchr(const char *str, int c)
 		if (*(str++) == c)
 			return ((char *) str);
 	return (NULL);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		len;
+	char	*dup;
+
+	len = ft_strlen(s);
+	dup = ft_calloc(len + 1, sizeof(char));
+	while (len--)
+		dup[len] = s[len];;
+	return (dup);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		len1;
+	int		len2;
+	char	*joined;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	joined = ft_calloc(len1 + len2 + 1, sizeof(char));
+	while (len2--)
+		joined[len1 + len2] = s2[len2];
+	while (len1--)
+		joined[len1] = s1[len1];
+	return (joined);
 }
